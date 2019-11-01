44a75c4c96bec32c96d6e51ad72afcc85c379cac
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2019-11-01 19:37:21 +0100

    Remove non-latest images

--- tf/modules/networking/main.tf
+++ tf/modules/networking/main.tf
@@ -216,3 +216,27 @@ resource "aws_ecr_repository" "simple_app" {
   name                 = "simple-app"
   image_tag_mutability = "MUTABLE"
 }
+
+resource "aws_ecr_lifecycle_policy" "expire_untagged" {
+  repository = "${aws_ecr_repository.simple_app.name}"
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire untagged images older than 1 day",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 1
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
