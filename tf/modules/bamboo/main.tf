fd95e1d81446cba6c3f0ec5b788b2a4a07950a34
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2019-11-01 19:15:35 +0100

    Allow bamboo to push images

--- tf/modules/bamboo/main.tf
+++ tf/modules/bamboo/main.tf
@@ -10,3 +10,38 @@ resource "aws_iam_instance_profile" "bamboo_instance_profile" {
   name                 = "Bamboo"
   role                 = "${aws_iam_role.bamboo_role.name}"
 }
+
+resource "aws_iam_policy" "bamboo" {
+  name                 = "Bamboo"
+  path                 = "/"
+  policy               = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PushSimpleAppImage",
+            "Effect": "Allow",
+            "Action": [
+                "ecr:CompleteLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:InitiateLayerUpload",
+                "ecr:PutImage",
+                "ecr:BatchCheckLayerAvailability"
+            ],
+            "Resource": "arn:aws:ecr:*:*:repository/simple-app"
+        },
+        {
+            "Sid": "GetAuthorizationToken",
+            "Effect": "Allow",
+            "Action": "ecr:GetAuthorizationToken",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "bamboo" {
+  role                 = "${aws_iam_role.bamboo_role.name}"
+  policy_arn           = "${aws_iam_policy.bamboo.arn}"
+}
