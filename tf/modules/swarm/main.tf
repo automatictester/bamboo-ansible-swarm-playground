417e9b2e3ac6412d8d01125d0e90f039ec0c2473
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2019-11-02 11:51:27 +0100

    Allow swarm host pull docker images from ecr

--- tf/modules/swarm/main.tf
+++ tf/modules/swarm/main.tf
@@ -10,3 +10,35 @@ resource "aws_iam_instance_profile" "swarm_instance_profile" {
   name                 = "Swarm"
   role                 = "${aws_iam_role.swarm_role.name}"
 }
+
+resource "aws_iam_policy" "swarm" {
+  name                 = "Swarm"
+  path                 = "/"
+  policy               = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PullSimpleAppImage",
+            "Effect": "Allow",
+            "Action": [
+                "ecr:BatchGetImage",
+                "ecr:GetDownloadUrlForLayer"
+            ],
+            "Resource": "arn:aws:ecr:*:*:repository/simple-app"
+        },
+        {
+            "Sid": "GetAuthorizationToken",
+            "Effect": "Allow",
+            "Action": "ecr:GetAuthorizationToken",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "swarm" {
+  role                 = "${aws_iam_role.swarm_role.name}"
+  policy_arn           = "${aws_iam_policy.swarm.arn}"
+}
